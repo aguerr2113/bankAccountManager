4aab0179531bfa1f303b3633b628d800ac8e2b58
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-04-04 16:02:24 -0500

    Implement basic BankAccountManager with deposit, withdraw, and balance retrieval

--- BankAccountManager.java
+++ BankAccountManager.java
@@ -1,5 +1,30 @@
 public class BankAccountManager {
-    public static void main(String[] args) throws Exception {
-        
+    private double balance;
+
+    public BankAccountManager(){
+        balance = 0.0;
+    }
+
+    public void deposit(double amount){
+        if(amount > 0.00){
+            balance += amount;
+        }
+        else{
+            System.out.println("Deposit must be greater than $0.00");
+        }
+    }
+    public void withdraw(double amount){
+        if(amount > 0){
+            if (amount > balance) {
+                System.out.println("Insufficient Funds...");
+            }
+        }
+        else{
+            balance -= amount;
+        }
+    }
+
+    public double getBalance(){
+        return balance;
     }
 }
